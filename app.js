3d87d80c94334f9825d41bf3669b7efe7c052389
Mohammed Ali <mohammed.ali@rzolut.com>
2025-08-06 13:49:32 +0530

    removed listen

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
