2fcf42b376d0a9bb5a65919448c90f2343d8576d
Mohammed Ali <mohammed.ali@rzolut.com>
2025-08-06 13:34:29 +0530

    .env added

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root", //global_admin
-  port: 3306,
-  password: "Mdlala@5253", //09rQ}h7qDNZ9
-  database: "global_properties",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Test the connection
